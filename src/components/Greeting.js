5245a3bf9012b36b48c33bd3783e88c6b0b34af4
MaryyBo <marusya.bodrug@gmail.com>
2024-03-07 14:57:17 +0100

    Greeting state (task)

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,20 +4,41 @@ class Greeting extends React.Component {
     constructor(props) {
         super(props);
 
-            this.state = {
+        this.state = {
+            isGreeting: true //.1 cтворити змінну стейту (за аналогією з лампочкою)
 
-            }
         }
-    
+    }
+    //2. Зробити обробник події кліку на h1 (за аналогією з лампочкою)
     //тут можуть бути якісь обробники подій
 
-    render () {
+    switcherBye() {
+        this.setState({
+            isGreeting: !this.state.isGreeting
+        });
+
+        console.log(this.state.greet);
+
+    }
+
+    render() {
         // console.log(this.props);
-        
-        return <h1>Hello {this.props.name}</h1>
+
+        // 3.В залежності від значення стейту , вирахувати, що за текст ви показуєте (за аналогією з лампочкою)
+        // Не забути підписати h1 на обробник у пункті 2
+
+        const greetingText = this.state.isGreeting ? 'Hello' : 'Bye'; // cтворюємо змінну з обчисленням того, що має змінитись і додаємо в h1
+
+        return <h1 onClick={() => { this.switcherBye() }}>{greetingText} {this.props.name}</h1>
+
     }
 }
 
 
-
 export default Greeting;
+
+/*
+
+Доробити компоненту таким чином, щоб за натиснення на h1 текст "Hello" змінювався на "Bye"
+
+*/
