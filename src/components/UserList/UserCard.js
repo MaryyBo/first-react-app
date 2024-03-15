138264a7fc5812d7330d2bfd2095ca815e246fed
MaryyBo <marusya.bodrug@gmail.com>
2024-03-15 17:48:15 +0100

    PropTypes Welcome

--- src/components/UserList/UserCard.js
+++ src/components/UserList/UserCard.js
@@ -1,28 +1,41 @@
-/* Тут потрібно реалізувати логіку відмалювання одного єдиного користувача,
- цей єдиний користувач спускається в компоненту UserCard через пропси від 
- батьківського компонента UserList
- - аватар
- - прізвище ім'я
- - імейл
-
- */
+import React from 'react';
+import PropTypes from 'prop-types';
 import './style.css';
 
-function UserCard(props) {
+class UserCard extends React.Component {
 
-    const { user: { name: { first: firstName, last: lastName }, email, picture: { large } }} = props;
-    return (
-        <article className='card-wrapper'>
-            <img src={large} alt={`${firstName}${lastName}`} className='user-img' />
-            <h1>{firstName} {lastName}</h1>
-            <p>{email}</p>
-        </article>
-    )
+
+
+    render() {
+        const { user: { name: { first, last }, email, picture: { large } } } = this.props;
+
+        return (
+            <article className='card-wrapper'>
+                <img src={large} alt={`${first}${last}`} className='user-img' />
+                <h1>{first} {last}</h1>
+                <p>{email}</p>
+            </article>
+        )
+    }
+}
+
+UserCard.propTypes = {
+    user: PropTypes.shape({
+
+        name: PropTypes.shape({
+            first: PropTypes.string.isRequired,
+            last: PropTypes.string.isRequired
+        }).isRequired,
+
+        email: PropTypes.string.isRequired,
+
+        picture: PropTypes.shape({
+            large: PropTypes.string.isRequired
+        }).isRequired
+
+    }).isRequired
 }
 
 export default UserCard;
 
 
-// Задача 2
-// В компоненті UserList передбачити можливість відмалювання карток для багатьох юзерів
-// https://randomuser.me/api/?results=100
--- src/components/app.js
+++ src/components/app.js
@@ -4,10 +4,11 @@ import SignInForm from "./SignInForm/SignInForm";
 import HiForm from "./HiForm/HiForm";
 import HelloParent from "./HelloForm/HelloParent";
 import ToDoList from "./ToDoList/todolist";
+import UserCard from "./UserList/UserCard";
 
 class App extends React.Component {
     render() {
-        return <ToDoList/>
+        return <UserList />
     }
 }
 
