6fe99d3aa091bf4c5afd6b7453bbf5b5c3fdd04d
MaryyBo <marusya.bodrug@gmail.com>
2024-03-19 17:56:11 +0100

    React Router DOM welcome

---
+++ src/components/NotFounf/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+    return (
+        <>
+            <h1>404</h1>
+            <p>Page is not found</p>
+        </>
+    )
+};
+
+export default NotFound;
--- src/components/app.js
+++ src/components/app.js
@@ -1,50 +1,36 @@
 import React from "react";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
+import UserList from "./UserList/UserList";
+import ToDoList from "./ToDoList/todolist";
+import Counter from "./Counter/CounterPage";
+import AlohaDashboard from "./AlohaDashboard/AlohaDashboard";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home/Home";
+import NotFound from "./NotFounf/NotFound";
 
 class App extends React.Component {
   render() {
     return (
-      <>
+      <BrowserRouter >
+        <nav>
+          <ul>
+            <li><Link to='counter'>go to Counter component</Link></li>
+            <li><Link to='users'>go to UserList component</Link></li>
+            <li><Link to='todo'>go to ToDoList component</Link></li>
+            <li><Link to='aloha'>go to AlohaDashboard component</Link></li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/todo" element={<ToDoList />} />
+          <Route path="/aloha" element={<AlohaDashboard />} />
+          <Route path="/*" element={<NotFound />} />
 
-        <ToastContainer
-          position="top-center"
-          autoClose={5000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-          theme="colored"
-        />
-        <button onClick={() => { window.location.assign('https://uk.legacy.reactjs.org/docs/react-component.html#componentdidupdate') }}>Assign</button>
-        <button onClick={() => { window.location.reload() }}>Reload</button>
-        <button onClick={() => { window.location.replace('https://developer.mozilla.org/ru/docs/Web/Events') }}>Replace</button>
-
-        <p>Lorem ipsum lorem ipsum lorem</p>
-
-        <button onClick={() => {
-          window.navigator.clipboard.writeText('Lorem ipsum lorem ipsum lorem');
-          toast.success('🦄 Copied to clipboard');
-        }}>Click to copy text</button>
-
-        <button onClick={() => {
-          
-          window.navigator.clipboard.readText()
-            .then((text) => {
-              
-              console.log(text);
-            });
-        }}>Read text from buffer</button>
-
-        <button onClick={() => {
-          window.navigator.geolocation.getCurrentPosition((positionObject) => {
-            console.log(positionObject)})
-        }}>Get my current position</button>
-      </>
+        </Routes>
+      </BrowserRouter>
     );
   };
 }
