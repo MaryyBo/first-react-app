7479014d017938d8dd7edd82746e3f565a7b9344
MaryyBo <marusya.bodrug@gmail.com>
2024-03-26 12:38:51 +0100

    Formik Validation + Error message

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { SIGN_UP_SCHEMA } from '../../schemas';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 const initialState = {
     email: '',
@@ -12,22 +12,30 @@ const initialState = {
 
 function SignUpForm(props) {
 
-   const handleSubmitToFormik = (values, actions) => {
+    const handleSubmitToFormik = (values, actions) => {
         console.log(values)
         console.log('дії', actions)
         actions.resetForm();
     }
 
     return (
-        <Formik initialValues={initialState} onSubmit={handleSubmitToFormik}>
+        <Formik
+            initialValues={initialState}
+            onSubmit={handleSubmitToFormik}
+            validationSchema ={SIGN_UP_SCHEMA}>
+
             {(formikProps) => {
                 console.log(formikProps);
                 return (
-                    <Form >
+                    <Form style={{display: 'flex', flexDirection: 'column' }}>
                         <Field name='firstName' placeholder='Type your first name' />
+                        <ErrorMessage  name='firstName' component='p'/>
                         <Field name='lastName' placeholder='Type your last name' />
+                        <ErrorMessage  name='lastName' component='p'/>
                         <Field name='email' placeholder='Type your email' />
+                        <ErrorMessage  name='email' component='p'/>
                         <Field name='password' placeholder='Type your password' />
+                        <ErrorMessage  name='password' component='p'/>
 
                         <button type='submit'>Submit</button>
                     </Form>
--- src/schemas/index.js
+++ src/schemas/index.js
@@ -4,6 +4,6 @@ export const SIGN_UP_SCHEMA = yup.object({
     firstName: yup.string().required().min(1).max(50),
     lastName: yup.string().required().min(1).max(50),
     email: yup.string().required().email(),
-    password: yup.string().required().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/)
+    password: yup.string().required().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/, 'Password is not valid')
 }
 )
