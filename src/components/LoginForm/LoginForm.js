467dfab11e62c0bb252432fd06dcd5e107ba2849
MaryyBo <marusya.bodrug@gmail.com>
2024-03-26 12:13:35 +0100

    Formik Welcome

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { SIGN_UP_SCHEMA } from '../../schemas';
+import { Formik, Form, Field } from 'formik';
 
 const initialState = {
     email: '',
@@ -9,81 +10,31 @@ const initialState = {
 }
 
 
-class SignUpForm extends Component {
-    constructor(props) {
-        super(props);
+function SignUpForm(props) {
 
-        this.state = {
-            ...initialState,
-            isError: null
-        }
+   const handleSubmitToFormik = (values, actions) => {
+        console.log(values)
+        console.log('дії', actions)
+        actions.resetForm();
     }
 
+    return (
+        <Formik initialValues={initialState} onSubmit={handleSubmitToFormik}>
+            {(formikProps) => {
+                console.log(formikProps);
+                return (
+                    <Form >
+                        <Field name='firstName' placeholder='Type your first name' />
+                        <Field name='lastName' placeholder='Type your last name' />
+                        <Field name='email' placeholder='Type your email' />
+                        <Field name='password' placeholder='Type your password' />
 
-
-    changeHandler = ({ target: { value, name } }) => {
-        this.setState({
-            [name]: value // квадратні дужки тут для того, щоб динамічно визначити ім'я властивості, яку потрібно оновити у стані, використовуючи значення name, яке ви отримали з події onChange
-        })
-    }
-
-    submitHandler = (event) => {
-        event.preventDefault();
-        try {
-            SIGN_UP_SCHEMA.validateSync(this.state); // потенційно тут робиться запит на сервер
-
-            // якщо стейт пройшов валідацію то отримуємо userObject і продовжуємо далі виконувати код  
-            this.setState({
-                isError: null
-            })
-        }// якщо стейт не пройшов валідацію, то попадаємо в catch
-        catch (err) {
-            this.setState({
-                isError: err
-            })
-        }
-    }
-    render() {
-
-        const { email, password, firstName, lastName, isError } = this.state;
-
-        return (
-            <form onSubmit={this.submitHandler}>
-                <input
-                    value={firstName}
-                    type='text'
-                    placeholder='Type your name'
-                    name='firstName'
-                    onChange={this.changeHandler}
-                />
-                <input
-                    value={lastName}
-                    type='text'
-                    placeholder='Type your lastname'
-                    name='lastName'
-                    onChange={this.changeHandler}
-                />
-                <input
-                    value={email}
-                    type='text'
-                    placeholder='Type your email'
-                    name='email'
-                    onChange={this.changeHandler}
-                />
-
-                <input
-                    value={password}
-                    type='text'
-                    placeholder='Type your password'
-                    name='password'
-                    onChange={this.changeHandler}
-                />
-                <button>Login</button>
-                {isError && <p style={{ color: 'red', fontSize: '20px' }}>{isError.message}</p>}
-
-            </form>
-        );
-    }
+                        <button type='submit'>Submit</button>
+                    </Form>
+                );
+            }}
+        </Formik>
+    );
 }
 
 export default SignUpForm;
