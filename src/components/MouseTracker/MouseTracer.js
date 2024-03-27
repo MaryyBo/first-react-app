54f7a526a5228781578810d3e16632817c66ac96
MaryyBo <marusya.bodrug@gmail.com>
2024-03-27 18:11:28 +0100

    Best Practice

--- src/components/MouseTracker/MouseTracer.js
+++ src/components/MouseTracker/MouseTracer.js
@@ -12,7 +12,10 @@ const MouseTracer = (props) => {
         y: 0
     })
 
+    const [counter, setCounter] = useState (0)
+
     useEffect(() => {
+
         window.addEventListener('mousemove', trackerHandler);
 
         return () => { // сlean function = componentWillUnmount
@@ -25,7 +28,7 @@ const MouseTracer = (props) => {
    // якщо ми хочему запустити useEffect і виконати його лише 1 раз, то ми маємо лишити пустий масив [] dependenciesList - як componentDidMount
 
     const trackerHandler = (event) => {
-        console.log(event)
+       
         const { clientX, clientY } = event
         setCoordinates({
             x: clientX,
@@ -34,10 +37,15 @@ const MouseTracer = (props) => {
 
     }
 
+    const clickHandler = () => {
+        setCounter (counter + 1)
+    }
     return (
         <>
             <p>X: {coordinates.x}</p>
             <p>Y: {coordinates.y}</p>
+            <button onClick={clickHandler}>click on me</button>
+            <p>Counter: {counter}</p>
         </>
     );
 }
