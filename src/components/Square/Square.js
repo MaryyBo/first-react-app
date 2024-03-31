f4faa30018ab3d8d422c7a26c1c089286e62a2ce
MaryyBo <marusya.bodrug@gmail.com>
2024-03-31 14:43:04 +0200

    useRef

---
+++ src/components/Square/Square.js
@@ -0,0 +1,57 @@
+
+/*Задача 1
+Ми вводимо число десь на сторінці відображається квадрат цього числа (нам треба зберігати десь число)
+
+*/
+
+import { useState } from 'react';
+import { useRef } from 'react';
+
+const Square = () => {
+
+    const [number, setNumber] = useState(1);
+    const refContainer = useRef(null)
+    console.log(refContainer)
+
+    const changeHandler = ({ target: { value } }) => {
+        setNumber(Number(value));
+
+        const {current} = refContainer;
+        if (value >=1 && value <= 10 ) {
+            current.style.backgroundColor = 'green'
+        } else if (value >= 11 && value <= 30) {
+            current.style.backgroundColor = 'yellow' 
+        } else if (value > 30) {
+            current.style.backgroundColor = 'red'
+            current.style.color = 'white'
+        }else if( value < 0) {
+            current.style.backgroundColor = 'pink'
+            current.style.color = 'white'
+        }
+    }
+
+    return (
+        <>
+            <h1>Number square</h1>
+            <input
+                type='number'
+                placeholder='Enter number'
+                value={number}
+                onChange={changeHandler}
+                ref={refContainer}
+            />
+            <p>Square: {number ** 2}</p>
+        </>
+
+    );
+}
+
+export default Square;
+
+
+/* Задача 2
+Якщо користувач вводить число до 10 (включно) - інпут зелений
+Якщо користувач вводить число від 11 до 30 (включно) - інпут жовтий
+Якщо користувач вводить число більше 30 (включно) - інпут червоний, колір тексту білий
+Якщо користувач вводить від'ємне число - інпут рожевий, колір тексту білий
+*/
--- src/components/app.js
+++ src/components/app.js
@@ -3,51 +3,18 @@ import React from "react";
 import Tree from './Tree/Tree'
 import { ThemeContext } from "../contexts/ThemeContext";
 import { UserContext } from "../contexts/userContext";
+import Square from "./Square/Square";
 import CONSTANTS from "../Constants";
 const { THEMES } = CONSTANTS;
 
+
+
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            user: {
-                firstName: 'John',
-                lastName: 'Doe',
-                email: 'johndoe@gmail.com',
-                avatar: 'https://cdn.britannica.com/92/212692-050-D53981F5/labradoodle-dog-stick-running-grass.jpg'
-            },
-            theme: THEMES.LIGHT
-        }
-    }
-
-    logOut = () => {
-        this.setState({
-            user: {}
-        });
-    }
-
-    setTheme = theme => {
-        this.setState({
-            theme
-        });
-    }
-    
+   
     render() {
 
         return (
-            <ThemeContext.Provider value={{
-                theme: this.state.theme,
-                setTheme: this.setTheme
-            }}>
-                <UserContext.Provider value={{
-                    user: this.state.user,
-                    logOut: this.logOut
-                }}>
-                    App
-                    <Tree />
-                </UserContext.Provider>
-            </ThemeContext.Provider>
+            <Square/>
         )
     }
 }
