eaab485f59715839dfb02581ebe211c63c2d5f84
MaryyBo <marusya.bodrug@gmail.com>
2024-04-01 21:21:57 +0200

    useReducer welcome (decrement added)

--- src/components/RegistrationForm/RegistartionForm.js
+++ src/components/RegistrationForm/RegistartionForm.js
@@ -8,6 +8,12 @@ function reducer(state, action) {
                 count: state.count + 1
             };
         }
+        case 'CLICK_DECREMENT': {
+            return {
+                ...state,
+                count: state.count - 1
+            };
+        }
         case 'INPUT_CHANGE': {
             // 1. Розбираємось з атрибутом name
             // 2. Коли ми розібралися, в який саме стейт має зайти значення - кладемо у відповідний стейт значення
@@ -27,19 +33,38 @@ const initialState = {
 const Clicker = () => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
-    const clickHandler = () => {
+    const clickIncrementHandler = () => {
         dispatch({
             type: 'CLICK_INCREMENT'
         }); // В dispatch ви передаєте об'єкт action
     }
 
+    const clickDecrementHandler = () => {
+        dispatch({
+            type: 'CLICK_DECREMENT'
+        });
+    }
+
     return (
         <>
             <h1>{state.count}</h1>
-            <button onClick={clickHandler}>Increment</button>
+            <button onClick={clickIncrementHandler}>Increment</button>
+            <button onClick={clickDecrementHandler}>Decrement</button>
         </>
     );
 }
+
+/*
+
+Задача
+
++ Додати декремент лічильника до функціоналу
+
++ 1. Пропишіть відповідний case у редьюсері
++ 2. Зробіть обробник події натиснення (onClick) кнопки декременту
++ 3. Створіть у верстці кнопку і прикрутіть до неї обробник події, який ви зробили на попередньому кроці
+
+*/
 
 export default Clicker;
 
