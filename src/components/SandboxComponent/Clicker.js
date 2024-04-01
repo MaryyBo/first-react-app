3c5114e82d742403e4ee6fca462b8698496cd08b
MaryyBo <marusya.bodrug@gmail.com>
2024-04-01 14:09:10 +0200

    useCallback practice task

--- src/components/SandboxComponent/Clicker.js
+++ src/components/SandboxComponent/Clicker.js
@@ -9,6 +9,7 @@ import React, { useState, useCallback } from 'react';
 const Clicker = () => {
 
     const [clickCount, setClickCount] = useState(0);
+    const [inputValue, setInputValue] = useState('')
 
     // const clickHandler = () => {
     //     setClickCount(clickCount + 1);
@@ -18,10 +19,46 @@ const Clicker = () => {
         setClickCount(clickCount => clickCount + 1);
     }, [])
 
+    // const changeHandler = ({ target: { value } }) => {
+    //     setInputValue(Number(value));
+    // }
+
+    const changeHandler = useCallback(({ target: { value } }) => {
+        setInputValue(Number(value));
+    })
+
+
+    // const logValueHandler = () => {
+
+    //     let sum = 0
+    //     for (let i = 0; i < inputValue; i++) {
+    //         sum += i
+    //     }
+    //     console.log(sum)
+    //     console.log(inputValue)
+    // }
+
+    const logValueHandler = useCallback(() => {
+        let sum = 0
+        for (let i = 0; i < inputValue; i++) {
+            sum += i
+        }
+        console.log(sum)
+        console.log(inputValue)
+        
+    }, [inputValue])
+
     return (
         <>
             <h1>Count: {clickCount}</h1>
             <button onClick={clickHandler}>Click</button>
+            <input
+                type='text'
+                value={inputValue}
+                onChange={changeHandler}
+                placeholder='Введіть число, для якого потрібно знайти суму чисел(верхню межу)'
+            />
+            <button onClick={logValueHandler}>Розрахувати суму</button>
         </>
     );
 }
